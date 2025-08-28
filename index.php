48e92710332c9014204c0ef3b9b49e9e693a6f14
daly <tu_email@example.com>
2025-08-28 11:33:37 +0200

    fondillo

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ if (file_exists($lang_file)) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="homepage-background">
     <img id="logo" src="images/logo1.png" alt="Logo">
     <div class="background-container"></div>
     <div class="language-selector">
